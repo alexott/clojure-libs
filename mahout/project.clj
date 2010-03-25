69ed49a608b44153cb4e21f5ec057a8b6db957e9
Alex Ott <alexott@gmail.com>
2010-03-25 15:18:48 +0100

    starting to play with mahout bindings

---
+++ mahout/project.clj
@@ -0,0 +1,7 @@
+(defproject clj-mahout "1.0.0-SNAPSHOT"
+  :description "Interface to Apache Mahout"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [org.apache.mahout/mahout "0.3"]
+                 ]
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
