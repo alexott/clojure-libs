8c15cb10723dcfca1e91a4f182f27952cd445210
Alex Ott <alexott@gmail.com>
2010-03-25 16:35:53 +0100

    add readme

--- mahout/project.clj
+++ mahout/project.clj
@@ -2,6 +2,6 @@
   :description "Interface to Apache Mahout"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.apache.mahout/mahout "0.3"]
+                 [org.apache.mahout/mahout-core "0.3"]
                  ]
   :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
